e162dcf3d6afe2bc2b2872e68321aee321155de4
KAPTOWE4KA <65016907+KAPTOWE4KA@users.noreply.github.com>
2022-06-13 18:00:21 +0600

    try2

--- main.py
+++ main.py
@@ -25,9 +25,28 @@ app = Flask(__name__)
 def index():
     # TODO оболочка сделана. Осталось сделать парсер более гибким и сделать взаимодействие с базой отдельно.
     # TODO а потом добавить рутов и их гет-пост обработку
-    #nav = open("templates/nav.html", encoding="utf-8").read()
-    return render_template("index.html")#, nav=nav)
+    nav = open("templates/nav.html", encoding="utf-8").read()
+    return render_template("Главная.html", nav=nav)
 
+
+@app.route("/parser/", methods=['GET'])
+def parser():
+    nav = open("templates/nav.html", encoding="utf-8").read()
+    return render_template("Парсер.html", nav=nav)
+
+
+@app.route("/parser/", methods=['POST'])
+def parser_post():
+    print(request.form['genre'])
+    print(request.form['count'])
+    nav = open("templates/nav.html", encoding="utf-8").read()
+    return render_template("Локальная-база-SQLite.html", nav=nav)
+
+
+@app.route("/table/")
+def table():
+    nav = open("templates/nav.html", encoding="utf-8").read()
+    return render_template("Локальная-база-SQLite.html", nav=nav)
 
 
 if __name__ == "__main__":
